f3f024397b806a0a4d38d104913de8c99d5db9fd
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-19 14:37:56 +0300

    connect to database

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors()); // make this secure later
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to mongoDB... ✅"))
+  .catch((err) =>
+    console.error("Error connecting to mongoDB... ❌", err.message)
+  );
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} 🌍`);
+  console.log(`Accessible at http://YOUR_IP:${port} 🖥️`);
+  
+});
