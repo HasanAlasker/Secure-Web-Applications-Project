6d2c006ae6f2c8ef8b9766d3b89c126cb532b213
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-19 15:10:53 +0300

    basic user route

---
+++ Server/routers/users.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+// user registration
+
+// user login
+
+// get all users (admin)
+
+// get me (auth users)
+
+export default router;
