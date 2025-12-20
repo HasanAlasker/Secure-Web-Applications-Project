702dcdcba0602cc8713c080633196cae1c4e1942
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-20 14:36:05 +0300

    basic routers in front end

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,30 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { getAllUsers } from "./api/user";
-import useApi from "./hooks/useApi";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login.Jsx";
 
 export default function App() {
-  const { data: users, request: fetchUsers, loading } = useApi(getAllUsers);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  if (loading) console.log("fetching users...");
-
-  const admins = users.filter((user) => user.role === "admin");
-  const normalUsers = users.filter((user) => user.role === "user");
-
-  const adminList = admins.map((user) => user.name);
-  const userList = normalUsers.map((user) => user.name);
-
   return (
-    <>
-      <h1>Admins: </h1>
-      <h2>{adminList.length > 0 ? adminList : "There are no admins"}</h2>
-      <br />
-      <h1>Users: </h1>
-      <h2>{userList.length > 0 ? userList : "There are no users"}</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- Client/src/pages/Admin.jsx
+++ Client/src/pages/Admin.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./App.css";
+import { getAllUsers } from "./api/user";
+import useApi from "./hooks/useApi";
 
 export default function Admin() {
+  const { data: users, request: fetchUsers, loading } = useApi(getAllUsers);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) console.log("fetching users...");
+
+  const admins = users.filter((user) => user.role === "admin");
+  const normalUsers = users.filter((user) => user.role === "user");
+
+  const adminList = admins.map((user) => user.name);
+  const userList = normalUsers.map((user) => user.name);
+
   return (
-    <div>Admin</div>
-  )
+    <>
+      <h1>Admins: </h1>
+      <h2>{adminList.length > 0 ? adminList : "There are no admins"}</h2>
+      <br />
+      <h1>Users: </h1>
+      <h2>{userList.length > 0 ? userList : "There are no users"}</h2>
+    </>
+  );
 }
