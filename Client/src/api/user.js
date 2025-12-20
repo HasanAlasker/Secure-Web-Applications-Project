10177fc08a64ad62969aabb00bee42f5c9f1c998
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-20 17:32:11 +0300

    basic login works, and better error handling on api endpoints

--- Client/src/api/user.js
+++ Client/src/api/user.js
@@ -1,28 +1,49 @@
+// src/api/user.js
 import api from "./axios";
 
 const endPoint = "/api/users";
 
 export const getAllUsers = async () => {
-  const res = await api.get(`${endPoint}/`);
-  return { ok: true, data: res.data };
+  try {
+    const res = await api.get(`${endPoint}/`);
+    return { ok: true, data: res.data };
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
 };
 
 export const getMe = async () => {
-  const res = await api.get(`${endPoint}/me`);
-  return { ok: true, data: res.data }; 
+  try {
+    const res = await api.get(`${endPoint}/me`);
+    return { ok: true, data: res.data };
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
 };
 
 export const loginUser = async (data) => {
-  const res = await api.post(`${endPoint}/login`, data);
-  return { ok: true, data: res.data }; 
+  try {
+    const res = await api.post(`${endPoint}/login`, data);
+    return { ok: true, data: res.data };
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
 };
 
 export const registerUser = async (data) => {
-  const res = await api.post(`${endPoint}/register`, data);
-  return { ok: true, data: res.data }; 
+  try {
+    const res = await api.post(`${endPoint}/register`, data);
+    return { ok: true, data: res.data };
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
 };
 
 export const logoutUser = async () => {
-  const res = await api.post(`${endPoint}/logout`);
-  return { ok: true, data: res.data }; 
+  try {
+    const res = await api.post(`${endPoint}/logout`);
+    return { ok: true, data: res.data };
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
 };
